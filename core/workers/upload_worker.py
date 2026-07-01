9d4d4b3a3e8d0adde0dce0dd692bdb49416463a1
hasanbasuwala <hasanbasuwala50@gmail.com>
2026-07-01 23:21:25 +0530

    Update upload_worker.py

--- core/workers/upload_worker.py
+++ core/workers/upload_worker.py
@@ -1,53 +1,18 @@
-import asyncio
 import logging
-
-from pyrogram.errors import FloodWait
 
 from core.state.queues import queues
 from core.state.registry import registry
 from core.state.models import JobStage
 
+from core.uploader.telegram import (
+    TelegramUploader
+)
+
 
 logger = logging.getLogger(__name__)
 
 
-async def safe_upload(app, job):
-
-    for attempt in range(5):
-
-        try:
-
-            await app.send_video(
-
-                chat_id=job.user_id,
-
-                video=str(
-                    job.output_file
-                )
-            )
-
-            return True
-
-        except FloodWait as e:
-
-            logger.warning(
-                f"Flood wait {e.value}"
-            )
-
-            await asyncio.sleep(
-                e.value
-            )
-
-        except Exception:
-
-            await asyncio.sleep(10)
-
-    return False
-
-
 async def upload_worker(app):
-
-    logger.info("Upload worker started")
 
     while True:
 
@@ -60,9 +25,11 @@ async def upload_worker(app):
                 JobStage.UPLOADING
             )
 
-            success = await safe_upload(
-                app,
-                job
+            success = await (
+                TelegramUploader.upload(
+                    app,
+                    job
+                )
             )
 
             if not success:
@@ -76,15 +43,9 @@ async def upload_worker(app):
                 JobStage.COMPLETED
             )
 
-            logger.info(
-                f"Upload complete {job.job_id}"
-            )
-
         except Exception as e:
 
-            logger.exception(
-                f"Upload failed {job.job_id}"
-            )
+            logger.exception(e)
 
             await registry.set_error(
                 job.job_id,
